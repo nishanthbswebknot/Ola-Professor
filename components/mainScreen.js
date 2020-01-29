bbcccb9c89f77b13d087e95537c9cae0f603a216
nishanthbswebknot <nishanth@gowebknot.com>
2020-01-29 16:00:41 +0530

    second commit

--- components/mainScreen.js
+++ components/mainScreen.js
@@ -1,22 +1,33 @@
 import React from 'react';
 import { StyleSheet, Text, View,Image,TouchableOpacity } from 'react-native';
 import { render } from 'react-dom';
-
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
 export default class mainScreen extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+          isReady: false,
         };
+      }
+      async componentDidMount() {
+       await Font.loadAsync({
+          'open-sans-bold': require('../assets/Roboto-Bold.ttf'),
+        });
+        this.setState({ isReady: true });
       }
       
      render(){
+      if (!this.state.isReady) {
+        return <AppLoading />;
+      }
   return (
     <View style={styles.container}>
      <Image
           style={{width: 350, height: 350,marginBottom:10}}
           source={require('../assets/coverpage.png')}
         />
-        <Text style={{marginBottom:10,fontWeight: 'bold',fontSize:45,alignItems: 'center',justifyContent: 'center'}}> Olá Professor </Text>
+        <Text style={{marginBottom:10,fontSize:35,alignItems: 'center',justifyContent: 'center',fontFamily: 'open-sans-bold'}}> Olá Professor </Text>
 <Text style={{marginBottom:20,fontWeight:'bold',alignItems: 'center',justifyContent: 'center',}}>Escolha uma das opções abaixo</Text>
 <View style={{flexDirection:"row",}}>
         <TouchableOpacity onPress={() => this.props.navigation.navigate('ChatScreen1')}style={{height:150,width:150,borderRadius: 5,borderColor:'#DADADA',backgroundColor:'#ffffff',borderWidth:1,alignItems: 'center',
